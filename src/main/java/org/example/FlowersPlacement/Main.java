38fb4e85c16a29278924a6ce1ae31ac27de39e89
Dominika Windows <d0mi04@windows.com>
2025-07-12 17:17:53 +0200

    cane place flowers 605 task

---
+++ src/main/java/org/example/FlowersPlacement/Main.java
@@ -0,0 +1,8 @@
+package org.example.FlowersPlacement;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean canBePlaced = Solution.canPlaceFlowers(new int[]{0,0,0,0,0}, 4);
+        System.out.println(canBePlaced);
+    }
+}
