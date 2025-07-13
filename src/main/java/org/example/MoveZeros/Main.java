0f6f3d4e8a57acb792f9d64fa86269414962c07b
Dominika Windows <d0mi04@windows.com>
2025-07-13 21:59:24 +0200

    Move Zeroes 283 Task

---
+++ src/main/java/org/example/MoveZeros/Main.java
@@ -0,0 +1,9 @@
+package org.example.MoveZeros;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution.moveZeros(new int[]{0,1,0,3,12});
+        Solution.moveZeros(new int[]{0});
+        Solution.moveZeros(new int[]{0,0,4,6,5,5,0,0,98});
+    }
+}
