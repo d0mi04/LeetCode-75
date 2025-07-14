92cfbdfaadc95aecf42f3ef3ba77dcd621acd1fc
Dominika Windows <d0mi04@windows.com>
2025-07-14 23:48:59 +0200

    Task 392. Is Subsequence

---
+++ src/main/java/org/example/IsSubsequence/Solution.java
@@ -0,0 +1,17 @@
+package org.example.IsSubsequence;
+
+public class Solution {
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while(i < s.length() && j < t.length()) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
